2b7f8c31ea45e030e3b0a289664c12a68b40aad2
Alex Stepanov <stepanov@a9.com>
2013-06-11 15:19:26 -0700

    Final code for Lecture 8

--- code/lecture8/min.cpp
+++ code/lecture8/min.cpp
@@ -1,69 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <functional>
 #include "binary_counter.h"
-#include "list_pool.h"
 
-template <typename I, typename Compare>
-class op_min1_2 
-{
-private:
-  Compare cmp;
-  list_pool<I>* pool_p;
-public:
-  typedef typename list_pool<I>::list_type list_type;
-  typedef std::pair<I, list_type> argument_type;
-  op_min1_2(const Compare& cmp, list_pool<I>& pool) 
-    : cmp(cmp), pool_p(&pool) {}
-  argument_type combine(const argument_type& x, 
-			const argument_type& y) {
-    free_list(*pool_p, y.second);
-    return std::make_pair(x.first, (*pool_p).allocate(y.first, x.second));
-  }
-  argument_type operator()(const argument_type& x, 
-			   const argument_type& y) {
-    return cmp(*(y.first), *(x.first)) ? combine(y, x) : combine(x, y);
-  }
-};
+// TODO: implement min_element_binary, using binary_counter
 
+// - compares two iterators and returns the one
+//   pointing to the smallest element
 template <typename Compare>
-class compare_dereference
+class min_op 
 {
 private:
   Compare cmp;
 public:
-  compare_dereference(const Compare& cmp) : cmp(cmp) {}
+  min_op(const Compare& cmp) : cmp(cmp) {}
   template <typename I>
-  bool operator() (I x, I y) {
-    return cmp(*x, *y);
+  I operator()(const I& x, const I& y) {
+    return cmp(*y, *x) ? y : x;
   }
 };
 
 template <typename I, typename Compare>
 // requires I is a ForwardIterator
 // and Compare is a StrictWeakOrdering on ValueType(I)
-std::pair<I, I> min1_2_element(I first, I last, Compare cmp) {
-  typedef op_min1_2<I, Compare> op_t;
-  typedef typename list_pool<I>::list_type list_type;
-  list_pool<I> pool;
-  binary_counter<op_t> min_counter(op_t(cmp, pool),
-				   std::make_pair(last, pool.end()));
-  while (first != last) min_counter.add(std::make_pair(first++, pool.end()));
-  std::pair<I, list_type> tmp = min_counter.reduce();
-  list_type list = min_element_list(pool, tmp.second, 
-				    compare_dereference<Compare>(cmp));
-  I min2 = pool.is_end(list) ? tmp.first : pool.value(list);
-  return std::make_pair(tmp.first, min2);
+I min_element_binary(I first, I last, Compare cmp) {
+  binary_counter<I, min_op<Compare> > min_counter(min_op<Compare>(cmp), last);
+  while (first != last) min_counter.add(first++); 
+  return min_counter.reduce();
 }
 
 int main() {
+
     // plugin whatever numbers you want to test with
-  int data[] = {9, 13, 7, 124, 32, 1, 81, 111, 237, 417, 41, 42, 13, 14, 15, 99, 11, 4};
-  int* end = data + sizeof(data)/sizeof(int);
-  std::pair<int*, int*> min1_2 = min1_2_element(data, end, std::less<int>());
-  if (min1_2.first == end) {
-    std::cout << "No elements" << std::endl;
-  } else { 
-    std::cout << "Min is " << *(min1_2.first) << " " << *(min1_2.second) << std::endl;
-  }
+    int data[] = {9, 13, 7, 124, 32, 17, 8, 32, 237, 417, 41, 42, 13, 14, 15};
+    int* end = data + sizeof(data)/sizeof(int);
+    int* min = min_element_binary(data, end, std::less<int>());
+    if (min == end) {
+        std::cout << "No elements" << std::endl;
+    } else { 
+        std::cout << "Min is " << *min << std::endl;
+    }
 }
