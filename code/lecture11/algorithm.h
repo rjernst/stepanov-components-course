805d03dffea5df505d44a61620594ece8cfa98fa
Alex Stepanov <stepanov@a9.com>
2013-07-16 16:01:57 -0700

    Code prep for lecture 11

---
+++ code/lecture11/algorithm.h
@@ -0,0 +1,44 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#define InputIterator typename
+#define ForwardIterator typename
+#define RandomAccessIterator typename
+#define BidirectionalIterator typename
+#define Integral typename
+
+template <InputIterator I>
+inline 
+I successor(I x) { return ++x; }
+
+template <ForwardIterator I, Integral N> 
+N iota(I first, I last, N start = N(0), N step = N(1)) {
+  typedef typename std::iterator_traits<I>::value_type T;
+  while (first != last) {
+    *first = T(start);
+    start += step;
+    ++first;
+  }
+  return start;
+}
+
+template <ForwardIterator I> 
+inline
+void iota(I first, I last) {
+  typedef typename std::iterator_traits<I>::value_type T;
+  iota(first, last, T(0), T(1));
+}
+
+template <RandomAccessIterator I>
+void random_iota(I first, I last) {
+  iota(first, last);
+  std::random_shuffle(first, last);
+}
+
+template <BidirectionalIterator I>
+void reverse_iota(I first, I last) {
+  iota(first, last);
+  std::reverse(first, last);
+}
+
+#endif
---
+++ code/lecture11/list_pool.h
@@ -0,0 +1,166 @@
+#ifndef LIST_POOL_H
+#define LIST_POOL_H
+
+#include <vector>
+#include <cstddef>
+#include <iterator>
+
+// Requirements on T: semiregular. 
+// Requirements on N: integral
+template <typename T, typename N = std::size_t>
+class list_pool {
+public:
+  typedef N list_type;
+  typedef T value_type;
+
+private:
+
+  struct node_t {
+    T value; 
+    list_type next; 
+  };
+
+  std::vector<node_t> pool; 
+
+  node_t& node(list_type x) {
+    return pool[x - 1]; 
+  }
+  const node_t& node(list_type x) const {
+    return pool[x - 1]; 
+  }
+
+  list_type new_list() {
+    pool.push_back(node_t()); 
+    return list_type(pool.size());
+  }
+
+  list_type free_list;
+
+ public:
+  typedef typename std::vector<node_t>::size_type size_type;
+
+  list_type end() const {
+    return list_type(0);
+  }
+
+  bool is_end(list_type x) const {
+    return x == end();
+  }
+
+  bool empty() const { 
+    return pool.empty();
+  }
+
+  size_type size() const {
+    return pool.size();
+  }
+
+  size_type capacity() const {
+    return pool.capacity();
+  }
+
+  void reserve(size_type n) {
+    pool.reserve(n);
+  }
+
+  list_pool() {
+    free_list = end();
+  }
+
+  list_pool(size_type n) {
+    free_list = end();
+    reserve(n); 
+  }
+
+  T& value(list_type x) {
+    return node(x).value;
+  }
+
+  const T& value(list_type x) const {
+    return node(x).value;
+  }
+
+  list_type& next(list_type x) {
+    return node(x).next;
+  }
+  const list_type& next(list_type x) const {
+    return node(x).next;
+  }
+
+  list_type free(list_type x) {
+    list_type tail = next(x); 
+    next(x) = free_list; 
+    free_list = x; 
+    return tail; 
+  }
+
+  list_type free(list_type front, list_type back) {
+    if (is_end(front)) return end();
+    list_type tail = next(back);
+    next(back) = free_list;
+    free_list = front;
+    return tail; 
+  }
+
+  list_type allocate(const T& val, list_type tail) {
+    list_type list = free_list; 
+    if (is_end(free_list)) {
+      list = new_list();
+    } else {
+      free_list = next(free_list);
+    }
+    value(list) = val; 
+    next(list) = tail;
+    return list; 
+  }
+
+  // operations on queues:
+  // pop_front, push_front, push_back and free, etc 
+
+  typedef std::pair<list_type, list_type> pair_type;
+
+  bool empty(const pair_type& p) { return is_end(p.first); }
+
+  pair_type empty_queue() { return pair_type(end(), end()); }
+
+  pair_type pop_front(const pair_type& p) {
+    if (empty(p)) return p;
+    return pair_type(next(p.first), p.second);
+  }
+
+  pair_type push_front(const pair_type& p, const T& value) {
+    list_type new_node = allocate(value, p.first);
+    if (empty(p)) return pair_type(new_node, new_node);
+    return pair_type(new_node, p.second);
+  }
+
+  pair_type push_back(const pair_type& p, const T& value) {
+      list_type new_node = allocate(value, end());
+      if (empty(p)) return pair_type(new_node, new_node);
+      next(p.second) = new_node;
+      return pair_type(p.first, new_node);
+  }
+
+  void free(const pair_type& p) { free(p.first, p.second); }
+
+#include "list_pool_iterator.h"
+};
+
+template <typename T, typename N, typename Compare>
+typename list_pool<T, N>::list_type
+min_element_list(const list_pool<T, N>& pool, 
+		 typename list_pool<T, N>::list_type list,
+		 Compare cmp) {
+  if (pool.is_end(list)) return list;
+  typename list_pool<T, N>::list_type current_min = list;
+  list = pool.next(list);
+  while (!pool.is_end(list)) {
+    if (cmp(pool.value(list), pool.value(current_min))) {
+      current_min = list;
+    }
+    list = pool.next(list);
+  }
+  return current_min;
+}
+
+#endif
